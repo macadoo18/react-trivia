d9b2ff89bab24b5a64c5c2d621f21656e955e207
Mac Hale <machaledesign@gmail.com>
2025-02-27 17:34:28 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>
+          <span className='main-font'>HOW WELL DO YOU KNOW</span> <br />
+          <span className='calvin-font'>calvin and hobbes?</span>
+        </h1>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/TriviaBox.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TriviaBox.css'
+
+function TriviaBox() {
+  return (
+    <div className='trivia-container'></div>
+  )
+}
+
+export default TriviaBox;
